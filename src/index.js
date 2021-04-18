93d9f10f9f9ac4819b6fa060f9d636c2f87d6298
KeslieBox <kesliebox@gmail.com>
2021-04-18 14:50:02 -0500

    commit tree 239ffd6b1ee9c823dc1d0feb10481240928a61c5

--- src/index.js
+++ src/index.js
@@ -13,3 +13,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(results => {
+    console.log(results)
+    results.forEach(result => {
+      addToyCard(result)
+    })
+  })
+
+  
+
+  function addToyCard(result) {
+    const toyCollection = document.querySelector("#toy-collection")
+    const div = document.createElement("div")
+    div.className = "card"
+    const h2 = document.createElement("h2")
+    h2.innerText = result.name
+    const img = document.createElement("img")
+    img.src = result.image
+    img.className = "toy-avatar"
+    const p = document.createElement("p")
+    p.innerText = result.likes + " likes"
+    const button = document.createElement("button") 
+    button.className = "like-btn"  
+    toyCollection.appendChild(div) 
+    div.innerHTML += h2.outerHTML + img.outerHTML + p.outerHTML + button.outerHTML
+  }
